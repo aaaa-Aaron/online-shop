55bd29fc8df7fa1a7698797cba8cd325d098252d
aaron <13264705201@163.com>
2021-07-25 23:48:18 +0800

    初始化项目

---
+++ coupon/src/main/java/com/aaron/onlineshop/coupon/CouponApplication.java
@@ -0,0 +1,13 @@
+package com.aaron.onlineshop.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponApplication.class, args);
+    }
+
+}
